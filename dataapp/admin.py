5e28c69f2ad40ef8df2728c5c3076395fa21020e
Kiattisak18 <petety2655@gmail.com>
2026-02-19 11:19:18 +0700

    Register models in Django Admin

--- dataapp/admin.py
+++ dataapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from dataapp.models import Genus
+from dataapp.models import Species,KnowledgeInfo,Image,AdminUser
 # Register your models here.
+
+admin.site.register(Genus)
+admin.site.register(Species)
+admin.site.register(AdminUser)
+admin.site.register(KnowledgeInfo)
+admin.site.register(Image)
